7a49fc69a923e00ea28519701105152bb908f6a7
Tony Holroyd <tony.j.holroyd@gmail.com>
2020-04-02 19:21:12 +0100

    Use shipped ffmpeg and make some settings into environment variables

--- entry.sh
+++ entry.sh
@@ -1,8 +1,12 @@
-#!/bin/bash -xv
+#!/bin/bash
 
+CAM_OPTIONS=${CAM_OPTIONS:- --mode 4}
+BITRATE=${BITRATE:- 8000000}
 echo Live-stream secret: $YOUTUBE_KEY
+echo Camera bitrate is ${BITRATE}
+echo Camera options are ${CAM_OPTIONS}
 
-raspivid -o - ${CAM_OPTIONS:- -ae 32,0x00,0x8080FF -a 12 --mode 4} -t 0  -b 8000000 -g 40 | ffmpeg -nostats -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -thread_queue_size 1024 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r ${FPS:-25} rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+raspivid -o -  -t 0 $CAM_OPTIONS -b ${BITRATE} -g 40 --nopreview | ffmpeg -nostats -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -thread_queue_size 1024 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r ${FPS:-25} rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
 
 
 /bin/sleep 3m
